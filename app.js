731758b3d547fdcec91a8eee85ed6d520bb37352
dinever <dinever@live.cn>
2014-03-08 01:06:37 +0800

    init app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var http = require('http');
+var mongoose = require('mongoose');
+
+var app = express();
+
+app.configure(function(){
+    app.set('port', 8080);
+    app.set('views', __dirname + '/app/views');
+    app.set('view engine', 'jade');
+    app.locals.pretty = true;
+    //	app.use(express.favicon());
+    //	app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.cookieParser());
+    app.use(express.session({ secret: 'super-duper-secret-secret' }));
+    app.use(express.methodOverride());
+    app.use(express.static(__dirname + '/app/public'));
+});
+
+
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server started, listenging on port ' + app.get('port'));
+});
