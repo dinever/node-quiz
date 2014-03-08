f089d12be7b87ff1af592dd2c40554148a5738e7
dinever <dinever@live.cn>
2014-03-08 16:41:05 +0800

    basic nextQuestion implemented

--- public/js/node-quiz.js
+++ public/js/node-quiz.js
@@ -1,7 +1,7 @@
 $(function() {
     $(".answer").click(function () {
         var qid = $("#play").attr("qid");
-        console.log(this.value);
+        console.log('click!');
         $.ajax({
             type: "POST",
             data: {
@@ -13,8 +13,15 @@ $(function() {
                     alert("no more questions!");
                 }
                 else{
+                    var form = $("<form></form>").attr('qid', data.id);
+                    form.append("<legend>" + data.title + "</legend>");
+                    for(var i = 0; i < data.answers.length; i++ )
+                    {
+                        var input = $('<input class="btn btn-large btn-block answer" type="button"/>').attr('name', 'answer'+i).attr('value', data.answers[i]);
+                        form.append(input);
+                    }
+                    $("#question").html(form);
                     alert(data.title);
-                    $("#play").children("legend").html(data.title);
                 }
             },
             url: '/answerQuestion'
