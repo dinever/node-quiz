57ef53f76a28588057e56716d4e062f091c783a8
dinever <dinever@live.cn>
2014-03-08 14:43:35 +0800

    add index routes

--- app.js
+++ app.js
@@ -18,7 +18,10 @@ app.configure(function(){
     app.use(express.static(__dirname + "/public"));
 });
 
+var models = {};
 models.Question = require('./models/question').(mongoose).model;
+
+require('./routes')(app, models);
 
 http.createServer(app).listen(app.get('port'), function() {
     console.log('Express server started, listening on port ' + app.get('port'));
