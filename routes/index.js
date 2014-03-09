7fd120b4334e7f8dbd83488684f675a393fd7167
dinever <dinever@live.cn>
2014-03-09 11:16:28 +0800

    quizManager View

--- routes/index.js
+++ routes/index.js
@@ -65,10 +65,35 @@ module.exports = function(app, models){
         })
     });
 
-    app.get("/quizManager/login", function(req, res) {
+    app.get('/quizManager/login', function(req, res) {
         res.render("login.jade", {
             title: title
         });
     });
+
+    app.post('/quizManager/login', function(req, res) {
+        req.method = "get";
+        if(req.body.username === "demo" && req.body.password === "demo"){
+            res.redirect('/quizManager/');//req.session.originalRoute
+            req.session.logged = true;
+        } else {
+            res.redirect('/quizManager/login');
+        }
+    });
+
+    app.get('/quizManager/viewQuestions', function(req, res) {
+        if(!req.session.logged) {
+            req.session.originalRoute = req.path;
+            res.redirect('/quizManager/login');
+            return;
+        }
+        models.Question.find(function(err, questions) {
+            res.render('viewQuestions', {
+                title: title,
+                questions: questions
+            });
+        });
+    });
+
 
 };
